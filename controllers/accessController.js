d18e97111869633a3a6bc84517477bce55614f66
goverdhan-gaur <goverdhangaur02@gmail.com>
2021-05-14 11:56:37 -0400

    added access Controller

--- controllers/accessController.js
+++ controllers/accessController.js
@@ -12,6 +12,6 @@ app.controller("accessController", [
 				"https://account-d.docusign.com/oauth/auth?response_type=code&scope=signature&client_id=76d6c621-e1e4-4bc2-8576-d947a485af00&redirect_uri=http://localhost:5501"
 			);
 		};
-        $scope.run();
+		$scope.run();
 	},
 ]);
